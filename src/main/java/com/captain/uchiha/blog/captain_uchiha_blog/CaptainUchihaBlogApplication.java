103c0815be7689f4ce0c67a8b83b7829c3150cfd
captain_uchiha <pvikash075@gmail.com>
2025-06-28 23:04:41 +0530

    initial commit

---
+++ src/main/java/com/captain/uchiha/blog/captain_uchiha_blog/CaptainUchihaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.captain.uchiha.blog.captain_uchiha_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptainUchihaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptainUchihaBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/captain/uchiha/blog/captain_uchiha_blog/CaptainUchihaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.captain.uchiha.blog.captain_uchiha_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptainUchihaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
